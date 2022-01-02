dbc5d4f5e87527dc1bac2dbcef16475ba98b0f76
thepsy-casentinok <thepsy-casentinok@github.com>
2022-01-02 21:12:32 +0900

    Pods remove from git

--- Pods/Target Support Files/Pods-oround/Pods-oround-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_oround : NSObject
-@end
-@implementation PodsDummy_Pods_oround
-@end
--- Pods/Target Support Files/SwiftPromises/SwiftPromises-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftPromises : NSObject
-@end
-@implementation PodsDummy_SwiftPromises
-@end
